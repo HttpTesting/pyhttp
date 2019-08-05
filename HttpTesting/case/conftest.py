eea99ffe60501e506173448639d775d70b5dd477
lengyaohui <lengyaohui.bj@acewill.cn>
2019-08-05 17:42:40 +0800

    优化pytest-html

--- HttpTesting/case/conftest.py
+++ HttpTesting/case/conftest.py
@@ -43,16 +43,18 @@ Results部分在此设置.
 """
 @pytest.mark.optionalhook
 def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(3, html.th('Time', class_='sortable time', col='time'))
+    cells.insert(1, html.th('Description'))
+    cells.insert(2, html.th('Time', class_='sortable time', col='time'))
     # cells.insert(1,html.th("Test_nodeid"))
+    cells.pop(3)
     cells.pop()
 
 @pytest.mark.optionalhook
 def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(3, html.td(datetime.utcnow(), class_='col-time'))
+    cells.insert(1, html.td(report.description))
+    cells.insert(2, html.td(datetime.utcnow(), class_='col-time'))
     # cells.insert(1,html.td(report.nodeid))
+    cells.pop(3)
     cells.pop()
 
 @pytest.mark.hookwrapper
@@ -60,4 +62,4 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
     report.description = str(item.function.__doc__)
-    report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")   #设置编码显示中文
+    # report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")   #设置编码显示中文
